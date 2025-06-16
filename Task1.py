ed85f2874cd04e0a2d002235d2868f6d730e8a30
MANPREETJAGDEV <manpreet.jagdev@gmail.com>
2025-06-16 19:33:54 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter First Number :"))
+num2=int(input("Enter Second Number :"))
+print("Addition :" , num1 + num2)
+print("Subtraction :", num1 - num2 )
+print("Multipication :", num1 * num2)
+print("Division :", num1/num2)
