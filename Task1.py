4fc3936a3658446372266b2d96d5b5632aaf3e42
MANPREETJAGDEV <manpreet.jagdev@gmail.com>
2025-06-16 19:37:48 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,3 +1,5 @@
+# Program to Do Addition,Subtraction,Multiplication and Division of two numbers entered by the user
+
 num1=int(input("Enter First Number :"))
 num2=int(input("Enter Second Number :"))
 print("Addition :" , num1 + num2)
