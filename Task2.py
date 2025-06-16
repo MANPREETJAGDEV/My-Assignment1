3d857a7b9502db32461ba8ad2f35e5329b17925b
MANPREETJAGDEV <manpreet.jagdev@gmail.com>
2025-06-16 19:35:32 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,6 @@
+# Program to concatenate last name and first name entered by the user
+
+first_name=input("Enter Your First Name :")
+last_name=input("Enter Your Last Name :")
+concatenate1= first_name + ' ' +  last_name
+print("Hello",",",  concatenate1, "!"," Welcome to the Python Praogram.")
